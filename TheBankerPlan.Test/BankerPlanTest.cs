9e388f214b928672f29030b1c3dc85ddc382f8db
jesse <jessefortuin850@gmail.com>
2023-05-30 14:42:10 +0200

    Feat: Completed test setup, README.md, .gitignore

---
+++ TheBankerPlan.Test/BankerPlanTest.cs
@@ -0,0 +1,23 @@
+namespace TheBankerPlan.Test
+{
+    public class BankerPlanTest
+    {
+        [Theory]
+        [InlineData(100000, 1, 2000, 15, 1, true)]
+        [InlineData(100000, 1, 9185, 12, 1, false)]
+        [InlineData(100000000, 1, 100000, 50, 1, true)]
+        [InlineData(100000000, 1.5, 10000000, 50, 1, false)]
+        [InlineData(100000000, 5, 1000000, 50, 1, true)]
+        public void FortuneTests(int deposit, double interest, int withdraw, int numberOfYears, double inflaion, bool expected)
+        {
+            //arrange
+            IBankerPlan plan = new BankerPlan();
+
+            //act
+            var actual = plan.Fortune(deposit, interest, withdraw, numberOfYears, inflaion);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
