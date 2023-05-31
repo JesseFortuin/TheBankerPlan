fd5ba48a556825744ecd7f97f8b462d217511765
jesse <jessefortuin850@gmail.com>
2023-05-31 17:08:29 +0200

    Feat: Method implemented

--- TheBankerPlan.BL/BankerPlan.cs
+++ TheBankerPlan.BL/BankerPlan.cs
@@ -4,7 +4,45 @@
     {
         public bool Fortune(int f0, double p, int c0, int n, double i)
         {
-            throw new NotImplementedException();
+            int yearsPassed = 2;
+
+            double pPercent = p / 100;
+
+            double iPercent = i / 100;
+
+            var result = FortuneLooper(f0, pPercent, c0, n, iPercent, yearsPassed);
+
+            return result;
+        }
+
+
+        public bool FortuneLooper(double f0, double p, double c0, int n, double i, int yearsPassed)
+        {
+            var hit = false;
+
+            if (f0 < c0 && hit == false)
+            {
+                hit = false;
+
+                return hit;
+            }
+
+            if (yearsPassed == n)
+            {
+                hit = true;
+
+                return hit;
+            }
+
+            f0 = f0 + (p * f0) - c0;
+
+            c0 = c0 + (c0 * i);
+
+            f0 = Math.Truncate(f0);
+
+            c0 = Math.Truncate(c0);
+
+            return FortuneLooper(f0, p, c0, n, i, yearsPassed + 1);
         }
     }
 }
--- TheBankerPlan.Test/BankerPlanTest.cs
+++ TheBankerPlan.Test/BankerPlanTest.cs
@@ -8,13 +8,13 @@ namespace TheBankerPlan.Test
         [InlineData(100000000, 1, 100000, 50, 1, true)]
         [InlineData(100000000, 1.5, 10000000, 50, 1, false)]
         [InlineData(100000000, 5, 1000000, 50, 1, true)]
-        public void FortuneTests(int deposit, double interest, int withdraw, int numberOfYears, double inflaion, bool expected)
+        public void FortuneTests(int deposit, double interest, int withdraw, int numberOfYears, double inflation, bool expected)
         {
             //arrange
             IBankerPlan plan = new BankerPlan();
 
             //act
-            var actual = plan.Fortune(deposit, interest, withdraw, numberOfYears, inflaion);
+            var actual = plan.Fortune(deposit, interest, withdraw, numberOfYears, inflation);
 
             //assert
             Assert.Equal(expected, actual);
